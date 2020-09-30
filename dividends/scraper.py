8945918266587af3c000ff2121695cc59aa4f111
Bryan Houlton <brhoulton@gmail.com>
2020-09-30 16:56:01 -0400

    made scraper.py autoupdate the azure db

--- dividends/scraper.py
+++ dividends/scraper.py
@@ -5,15 +5,13 @@ from datetime import datetime, timedelta
 import yfinance
 
 
-with open('dividends/dividends.json', 'r') as file:
-    dataset = json.load(file)
-
 def scrapeTickers():
     
     DATE = datetime.now().strftime("%d-%b-%Y")
     URL = 'https://api.nasdaq.com/api/calendar/dividends?date=2020-09-30'
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-    
+    API_URL = 'https://rtstockdata.azurewebsites.net/dividend_update'
+
     dailyDataset = []
 
     response = requests.get(URL, headers = HEADERS).json()
@@ -27,10 +25,12 @@ def scrapeTickers():
         soldAt = hist.iloc[-1]['Open']
         profit = round(soldAt - boughtAt + row['dividend_Rate'], 2)
         volume = hist.iloc[-1]['Volume']
-        dataset.append({'ticker': row['symbol'], 'exdate': DATE, 'boughtAt': boughtAt, 'soldAt': soldAt, 'profit': profit, 'volume': volume, 'amount': row['dividend_Rate']})
+        dailyDataset.append({'ticker': row['symbol'], 'exdate': DATE.replace("-", ""), 'boughtAt': boughtAt, 'soldAt': soldAt, 'profit': profit, 'volume': volume, 'amount': row['dividend_Rate']})
         count += 1
+
+    
+    headers = {'Content-Type': "application/json", 'Accept': "application/json"}
+    APIResponse = requests.post(API_URL, headers = headers, json = {'hash':'7A6ECCDB792062797AA4F6BEE12199219EC8749641BA50B3138DD4857A65173D', 'data': dailyDataset})
+    print(APIResponse)
     
 scrapeTickers()
-
-with open('dividends/dividends.json', 'w') as file:
-    json.dump(dataset, file)
